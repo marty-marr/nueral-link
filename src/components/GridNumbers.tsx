2be7d9481435eef0cf89de8e6b44ff6ec1d1bdb4
Martin Martinez Jr <martinjr@Martins-MBP.frontiernet.net>
2026-09-08 19:27:35 -0700

    feat: clickable cells that highlight on click

--- src/components/GridNumbers.tsx
+++ src/components/GridNumbers.tsx
@@ -1,11 +1,21 @@
 import codeMatrixNumbers from "../Hooks/codeMatrixNumbers.ts";
+import {useState} from "react";
+
+
 
 
 function gridNumbers() {
+    const [selected, setSelected] = useState<number[]>([]);
+
+    const toggleSelect = (index:number) => {
+        setSelected((prev) => prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+        ) ;
+    }
+
     return (
         <div className="code-matrix-numbers">
             {codeMatrixNumbers.map((number, index) => (
-                <div key={index}>{number}</div>
+                <div key={index} className={selected.includes(index) ? 'cell active' : 'cell'} onClick={() => toggleSelect(index)}>{number}</div>
             ))}
         </div>
     )
